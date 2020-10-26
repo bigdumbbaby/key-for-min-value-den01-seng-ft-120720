e18d934947b0f6745fc82ce9eb6f2192fc9f006c
Colton O'Connor <colton.shawn.oconnor@gmail.com>
2020-10-26 23:39:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  min_value = nil
   name_hash.each do |item, value|
-    if 
-      
+    if value < min_value
+      min_value = value
     end
   end
 end
