f73cfacd50579077bebc886c0befad75219783bb
Colton O'Connor <colton.shawn.oconnor@gmail.com>
2020-10-26 23:43:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   min_value = 100
   output = ""
-  name_hash.each do |item, value|
+  name_hash.each do |key, value|
     if value < min_value
       min_value = value
       output = key
